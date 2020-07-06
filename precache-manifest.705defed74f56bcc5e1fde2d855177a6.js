d71190ad378dc22d0d718ae721ec98cbb4ec9409
Reny10 <Reny10@users.noreply.github.com>
2020-07-06 17:57:01 +0100

    Updates

---
+++ precache-manifest.705defed74f56bcc5e1fde2d855177a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f13ae88a729ae6b4c4f98075763a245",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "d0e1e23186c393703ff3",
+    "url": "/weather/static/css/main.b0cf79e8.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/weather/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e1e23186c393703ff3",
+    "url": "/weather/static/js/main.d220703b.chunk.js"
+  },
+  {
+    "revision": "b7586fcaead62fc35b8b",
+    "url": "/weather/static/js/runtime-main.d777cdb5.js"
+  },
+  {
+    "revision": "e53699030c9753fe146ac251c16767f5",
+    "url": "/weather/static/media/cold2.e5369903.jpg"
+  },
+  {
+    "revision": "69b97ecc460bd251feca338c27edc8fb",
+    "url": "/weather/static/media/sunny.69b97ecc.jpg"
+  }
+]);
